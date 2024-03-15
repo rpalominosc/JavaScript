f56f6423a7ed26f48d66c42659bb2a95517809d9
rpalominosc <r.palominos.c@gmail.com>
2024-03-15 17:37:54 -0300

    modifica arch suma_arreglo.js

--- Aprendizaje/Proyecto_1/suma_arreglo.js
+++ Aprendizaje/Proyecto_1/suma_arreglo.js
@@ -5,11 +5,15 @@ function range (inicio,fin,paso){
     else{
         avance = paso
     }
+    j=inicio;
     let arreglo = [];
     for ( i=inicio; i<=fin; avance){
-        arreglo[i-1]=i;
-        i=i+avance
+        arreglo.push(j);
+        i=i+avance;
+        j=i;
     }
     return arreglo;
 }
-console.log(range(1,10,3));
+sum=0;
+range(1,10).forEach((el) => sum += el);
+console.log(sum);
