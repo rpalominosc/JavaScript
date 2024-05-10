c66b7a0b94aa88a2a5721fc406750916bf4c4407
rpalominosc <r.palominos.c@gmail.com>
2024-05-10 17:21:15 -0400

    agrega 2 archivitos

---
+++ CursoFM/DOM/obtenerElementos.js
@@ -0,0 +1,16 @@
+// /*  get elento by id */
+// 
+// const contenedor1= document.getElementById('contenedor1');
+// console.log(contenedor1);
+// 
+// /*   children  */
+// console.log(contenedor1.children);
+
+/*   queryslector
+    Devuelve el primer elemento que coincida con un 
+    selector estilo css. (Devuelve un nodelist)
+
+*/
+
+const  caja = document.querySelector('#contenedor1 .caja');
+console.log(caja);
